4623f2ab3bb9565b4a337cbdd7ce598ceb246922
Pradnya <pradnya.bharambe@gmail.com>
2016-04-01 12:05:11 -0700

    Added files via upload

---
+++ career-form.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Qcentua Technologies</title>
+<link href="css/global.css" rel="stylesheet" type="text/css" />
+<script type="text/JavaScript" src="js/validation1.js"></script>
+<script type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+</script>
+<link href="custom-nivo-slider.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="stylesheet" href="nivo-slider.css" type="text/css" media="screen" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="jquery.nivo.slider.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(window).load(function() {
+	$('#slider').nivoSlider();
+});
+</script>
+<style type="text/css">
+<!--
+.style1 {color: #D72D0A}
+-->
+</style>
+</head>
+
+<body onload="MM_preloadImages('images/nav/01h.png','images/nav/02h.png','images/nav/03h.png','images/nav/04h.png','images/nav/05h.png','images/nav/06h.png','images/nav/07h.png','images/nav/08h.png')">
+<div id="wrapper">
+
+
+<div class="slidestab">
+		  <table width="179" height="332" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+              <td height="49" colspan="2"><img src="images/what.jpg" /></td>
+            </tr>
+            <tr>
+              <td width="131" height="60"><strong>JOB 1  :</strong><br />
+              <span class="font10">UNIX/Linux Sys. Adm.</span></td>
+              <td width="48" align="right"><a href="openings.html"></a></td>
+            </tr>
+            <tr>
+              <td height="62"><strong>JOB 2  :</strong><br />
+                <span class="font10">SAP Security Administrator</span></td>
+               <td width="48" align="right"><a href="openings.html"></a></td>
+            </tr>
+            <tr>
+              <td height="58"><strong>JOB 3  :</strong><br />
+                <span class="font10">Q/A TEST LEAD</span></td>
+               <td width="48" align="right"><a href="openings.html"></a></td>
+            </tr>
+            <tr>
+              <td height="67"><strong>JOB 4  :</strong><br />
+                <span class="font10">Senior Software Engineers</span></td>
+              <td width="48" align="right"><a href="openings.html"></a></td>
+            </tr>
+            <tr>
+              <td><strong>JOB 5  :</strong><br />
+                <span class="font10">Senior Datawarehouse Developer </span></td>
+              <td width="48" align="right"><a href="openings.html"></a></td>
+            </tr>
+          </table>
+		</div>
+
+
+	<div id="containar">
+		<div id="header">
+			<div class="logo">
+				<div class="logoimg">
+				<object data="images/logo2.swf" type="application/x-shockwave-flash" width="382" height="88">
+				<param name="allowScriptAccess" value="sameDomain" />
+				<param name="allowFullScreen" value="false" />
+				<param name="movie" value="images/logo2.swf" />
+				<param name="quality" value="high" />
+				<param name="menu" value="false" />
+				<param name="Wmode" value="transparent"/>
+				</object>
+				</div>
+				<div class="search">
+				  <form id="form1" name="form1" method="post" action="">
+				    <table width="230" height="40" border="0" align="right" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td width="151" align="center">Search : </td>
+                        <td width="149" align="left" valign="middle"><label>
+                          <input name="textfield" type="text" class="searchbox" />
+                        </label></td>
+                      </tr>
+                    </table>
+                  </form>
+			    </div>
+			</div>
+			<div class="ncvbar">
+			<ul>
+				<li><a href="index.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image3','','images/nav/01h.png',1)"><img src="images/nav/01.png" name="Image3" width="109" height="48" border="0" id="Image3" /></a></li>
+				<li><a href="about-qct.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image4','','images/nav/02h.png',1)"><img src="images/nav/02.png" name="Image4" width="132" height="48" border="0" id="Image4" /></a></li>
+				<li><a href="career.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image5','','images/nav/03h.png',1)"><img src="images/nav/03.png" name="Image5" width="102" height="48" border="0" id="Image5" /></a></li>
+				<li><a href="technologies.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image6','','images/nav/04h.png',1)"><img src="images/nav/04.png" name="Image6" width="149" height="48" border="0" id="Image6" /></a></li>
+				<li><a href="services.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image7','','images/nav/05h.png',1)"><img src="images/nav/05.png" name="Image7" width="143" height="48" border="0" id="Image7" /></a></li>
+				<li><a href="industries.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image8','','images/nav/06h.png',1)"><img src="images/nav/06.png" name="Image8" width="125" height="48" border="0" id="Image8" /></a></li>
+				<li><a href="clients.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image9','','images/nav/07h.png',1)"><img src="images/nav/07.png" name="Image9" width="116" height="48" border="0" id="Image9" /></a></li>
+				<li><a href="contact-us.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image10','','images/nav/08h.png',1)"><img src="images/nav/08h.png" name="Image10" width="124" height="48" border="0" id="Image10" /></a></li>
+			</ul>
+		  </div>
+		</div>
+		<div id="banner">
+		<div id="slider"> 
+	<img src="images/banner.png" alt="" />
+	<img src="images/banner1.png" alt="" />
+	<img src="images/banner2.png" alt="" /> 
+	<img src="images/banner3.png" alt="" />
+	</div>
+		</div>
+		<div id="contents">
+		
+			<img src="images/career.png" alt="Welcom" width="278" height="39" />
+		
+			<table width="959" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="center">
+				
+				<form action="contact.php" method="post" enctype="multipart/form-data" name="regform" onSubmit="return CheckRegister();">
+			<table width="950" border="0" cellspacing="0" cellpadding="0">
+              
+              <tr>
+                <td height="30" colspan="2" align="left">
+				<?php
+			if(isset($_POST['Submit']))
+			{
+			                 
+								
+							
+							$strTo="sreekumar@dotlinedesigns.com";
+							 $strSubject=" Qcentua Technologies Careers page " .$_POST[''];
+							 
+						
+
+							 $message = "\n".
+							 $message = '
+			
+			<table width="500" border="1" cellspacing="0" cellpadding="5">
+               
+				
+				
+				<tr>
+					<td>Name : <font color="#FF0000"> '.$_POST['name'].' </font></td>
+				</tr>
+				<tr>
+					<td>Email ID : <font color="#FF0000"> '.$_POST['email'].' </font></td>
+				</tr>
+				<tr>
+					<td>Phone Number: <font color="#FF0000"> '.$_POST['contact'].' </font></td>
+				</tr>
+				<tr>
+					<td>Resume : <font color="#FF0000"> '.$_POST['resume'].' </font></td>
+				</tr>
+				
+				
+				
+              </table>';
+                             
+								 $headers .= "From:".$_REQUEST['email'] ."\n";
+                                 $headers .= "MIME-version: 1.0\n";
+                                 $headers .= "Content-type: text/html; charset= iso-8859-1\n"; 
+                             $result = mail($strTo,$strSubject,$message,$headers);
+							 if ($result)
+							 {
+							 echo " 	
+                                   <p align='center'  class='colur1'><br/><b>Our Team member will get back to you shortly. Thank You.</b></p>";
+							 }
+							 else
+							 {
+							  echo " 	
+                                   <br><br><p align='center' class='colur1'>Sorry, Mail not sent. Please try again later.</p>";
+							 
+							 }
+							}
+							else
+							{
+							
+							
+			?>				</td>
+              </tr>
+              <tr>
+                <td height="30" colspan="2" align="left">&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="30" colspan="2" align="left"><?PHP
+	}
+?></td>
+                </tr>
+              <tr>
+                <td width="479" align="left">&nbsp;</td>
+                <td width="471" align="left">&nbsp;</td>
+              </tr>
+            </table>
+			</form>
+				</td>
+              </tr>
+              <tr>
+                <td align="center">&nbsp;</td>
+              </tr>
+              <tr>
+                <td align="center">&nbsp;</td>
+              </tr>
+            </table>
+			<p>&nbsp;</p>
+			
+      </div>
+	</div>
+</div>
+<div id="footer12">
+	<div class="tables12">
+	  <div class="copy12">
+			<div class="leftcon12">
+			<p><a href="index.html">Home </a>| <a href="about-qct.html">About QCT</a> | <a href="career.html">Career</a> | <a href="technologies.html">Technologies</a> | <a href="services.html">What We Do </a> | <a href="industries.html">Industries</a> | <a href="clients.html">Clients</a> | <a href="contact-us.html">Contact Us</a> <br />
+		    2012. Qcentum Technologies. All Rights Reserved </p>
+		</div>
+			<div class="rightcon12">
+
+				<p>Designed by <strong><a href="http://dotlinedesigns.com/" target="_blank">Dotline</a></strong></p>
+		</div>
+	  </div>
+  </div>
+</div>
+</body>
+</html>
